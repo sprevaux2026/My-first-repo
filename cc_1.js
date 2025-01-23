60b97717e52428e58b542f6d072618e348736be8
sprevaux2026 <stevenprevaux@gmail.com>
2025-01-23 18:48:02 -0500

    Task 4- Customer Messaging

--- cc_1.js
+++ cc_1.js
@@ -1,3 +1,4 @@
+//Task 1
 let employeename = "Steven" 
 const employeeID = 12345
 var isActive = true
@@ -5,12 +6,16 @@ console.log(employeename, typeof employeename)
 console.log(employeeID, typeof employeeID)
 console.log(isActive, typeof isActive)
 
+//Task 2
+
 let productName = "Laptop"
 const productPrice = "999.99"
 var isAvailable = true
 console.log(productName, typeof productName)
 console.log(productPrice, typeof productPrice)
 console.log(isAvailable, typeof isAvailable)
+
+//Task 3
 
 let accountBalance = 10000;
 console.log("Initial balance:", accountBalance); // Shows 10000
@@ -26,3 +31,9 @@ console.log("After multiplying by 4:", accountBalance);
 
 accountBalance /= 2;
 console.log("After dividing by 2:", accountBalance);
+
+//Task 4
+
+let customerName = "Jack Black"; // Fixed variable name
+let welcomeMessage = "Welcome to our store, " + customerName + ". We are so happy you picked us!";
+console.log(welcomeMessage);
