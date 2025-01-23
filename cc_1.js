0d7c3290b84fdc7c7de603dae3b0814569ef6813
sprevaux2026 <stevenprevaux@gmail.com>
2025-01-23 18:06:19 -0500

    Task 2- product Details

--- cc_1.js
+++ cc_1.js
@@ -4,3 +4,9 @@ var isActive = true
 console.log(employeename, typeof employeename) 
 console.log(employeeID, typeof employeeID)
 console.log(isActive, typeof isActive)
+let productName = "Laptop"
+const productPrice = 999.99
+var isAvailable = true
+console.log(productName, typeof productName)
+console.log(productPrice, typeof productPrice)
+console.log(isAvailable, typeof isAvailable)
