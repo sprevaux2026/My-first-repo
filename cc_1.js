b7bf2aa996c0fb84b509c1ae1dc2a61898881c42
sprevaux2026 <stevenprevaux@gmail.com>
2025-01-23 17:59:04 -0500

    Task 1- Employee Information

---
+++ cc_1.js
@@ -0,0 +1,6 @@
+let employeename = "Steven" 
+const employeeID = 12345
+var isActive = true
+console.log(employeename, typeof employeename) 
+console.log(employeeID, typeof employeeID)
+console.log(isActive, typeof isActive)
